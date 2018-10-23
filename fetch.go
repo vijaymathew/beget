44c3078c19ddcab0a0d01977953329896e28167d
Vijay Mathew <mathew.vijay@gmail.com>
2018-10-23 10:06:48 +0530

    fetch.Get - implemnted network call.

--- fetch.go
+++ fetch.go
@@ -2,20 +2,52 @@ package beget
 
 import (
 	"fmt"
+	"log"
+	"io/ioutil"
+	"net/http"
 )
 
-type HttpResponse struct {
-	Status string // e.g. 200 OK
-	StatusCode int // e.g. 200
-	Header map[string]string
+type BaseResponse struct {
+	StatusCode int // e.g -1, 200
 	Body string
 }
 
-func Get(url string) (response HttpResponse) {
-	fmt.Printf("GET %s\n", url)
-	response.Status = "200 OK"
-	response.StatusCode = 200
-	// TODO: fetch the contents of url, put that into
-	// and HttpResponse and return it.
+type HTTPResponse struct {
+	BaseResponse
+	Status string // e.g. 200 OK
+	Header map[string][]string
+}
+
+// Get fetches a document via an HTTP GET request.
+// The status, header and body information of the HTTP response
+// will be returned in an HTTPResponse object.
+// In case of an IO or system error, response.StatusCode will be set to -1
+// and response.Body will contain a description of the error.
+func Get(url string) (response HTTPResponse) {
+	res, err := http.Get(url)
+	response.StatusCode = -1
+	if err != nil {
+		log.Fatal(err)
+		response.Body = fmt.Sprintf("%s", err)
+		return
+	}
+	defer res.Body.Close()
+	bytes, err := ioutil.ReadAll(res.Body)
+	response.Body = string(bytes)
+	if err != nil {
+		log.Fatal(err)
+		response.Body = fmt.Sprintf("%s", err)
+		return
+	}
+	response.Status = res.Status
+	response.StatusCode = res.StatusCode
+	response.Header = make(map[string][]string)
+	for key, values := range res.Header {
+		newValues := make([]string, len(values))
+		for i, v := range values {
+			newValues[i] = v
+		}
+		response.Header[key] = newValues
+	}
 	return
 }
