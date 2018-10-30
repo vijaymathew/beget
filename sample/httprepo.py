900e424528cca5c0136e5fa60f86dc7ce326c3fa
Vijay Mathew <mathew.vijay@gmail.com>
2018-10-30 11:29:25 +0530

    Sample SimpleHTTP repository - first commit.

---
+++ sample/httprepo.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+app = Flask('httprepo')
+
+@app.route('/doc', methods=['POST'])
+def post_doc(uuid):
+    content = request.json
+    print(content)
+    return 'OK'
+
+app.run(host='localhost', port=5050)
